b4fd9dd27f27e26a1db2e1828604f55031159ede
periclesevt <periclesevt@gmail.com>
2025-05-12 21:34:58 -0300

    Create repository and UserRepository

---
+++ src/main/java/me/dio/santander_dev_week/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package me.dio.santander_dev_week.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.santander_dev_week.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
