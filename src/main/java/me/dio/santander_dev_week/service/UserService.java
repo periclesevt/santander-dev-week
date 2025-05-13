61df9572e74de7c4747b9b98638864f065d3493d
periclesevt <periclesevt@gmail.com>
2025-05-12 22:56:29 -0300

    add @Serial

--- src/main/java/me/dio/santander_dev_week/service/UserService.java
+++ src/main/java/me/dio/santander_dev_week/service/UserService.java
@@ -1,7 +1,6 @@
 package me.dio.santander_dev_week.service;
 
 import me.dio.santander_dev_week.domain.model.User;
-import me.dio.santander_dev_week.service.impl.UserServiceImpl;
 
 public interface UserService extends CrudService<Long, User> {
 
--- src/main/java/me/dio/santander_dev_week/service/exception/NotFoundException.java
+++ src/main/java/me/dio/santander_dev_week/service/exception/NotFoundException.java
@@ -1,7 +1,10 @@
 package me.dio.santander_dev_week.service.exception;
 
+import java.io.Serial;
+
 public class NotFoundException extends BusinessException {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public NotFoundException() {
