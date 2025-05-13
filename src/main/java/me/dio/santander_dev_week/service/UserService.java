b82c42f9cfaed534b349afc2e45ffa40d8cfdfda
periclesevt <periclesevt@gmail.com>
2025-05-12 22:28:40 -0300

    Create service and exceptions

---
+++ src/main/java/me/dio/santander_dev_week/service/UserService.java
@@ -0,0 +1,8 @@
+package me.dio.santander_dev_week.service;
+
+import me.dio.santander_dev_week.domain.model.User;
+import me.dio.santander_dev_week.service.impl.UserServiceImpl;
+
+public interface UserService extends CrudService<Long, User> {
+
+}
---
+++ src/main/java/me/dio/santander_dev_week/service/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package me.dio.santander_dev_week.service.exception;
+
+public class NotFoundException extends BusinessException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException() {
+        super("Resource not found.");
+    }
+
+}
